a4c244563bfa8ae603fa2f6b8e2339589f1ea945
╭•Subaru• 神 <115202562+Aran-Sama@users.noreply.github.com>
2023-05-17 23:59:43 +0530

    Update bot.py

--- VIVI/core/bot.py
+++ VIVI/core/bot.py
@@ -12,7 +12,7 @@ class VIVIBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            "VIVIMusic",
+            "VelionaMusicBot",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
@@ -36,7 +36,7 @@ class VIVIBot(Client):
         LOGGER(__name__).info(f"MusicBot Started as {self.name}")
         try:
             await self.send_message(
-                config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :**\n\n✨ ɪᴅ : `{self.id}`\n❄ ɴᴀᴍᴇ : {self.name}\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.username}"
+                config.LOG_GROUP_ID, f"**» Bot Started"
             )
         except:
             LOGGER(__name__).error(
