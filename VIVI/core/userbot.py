112e973e04ea1191598db6f8357d9c110612f812
╭•Subaru• 神 <115202562+Aran-Sama@users.noreply.github.com>
2023-05-18 00:18:52 +0530

    Update userbot.py

--- VIVI/core/userbot.py
+++ VIVI/core/userbot.py
@@ -48,8 +48,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("WoFBotsSupport")
-                await self.one.join_chat("WoFBotsSupport")
+                await self.one.join_chat("BotsXSupport")
+                await self.one.join_chat("BotsXSupport")
             except:
                 pass
             assistants.append(1)
@@ -68,7 +68,7 @@ class Userbot(Client):
             )
             try:
                 await self.one.send_message(
-                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ᴀssɪsᴛᴀɴᴛ ᴏɴᴇ sᴛᴀʀᴛᴇᴅ :**\n\n✨ ɪᴅ : `{self.one.id}`\n❄ ɴᴀᴍᴇ : {self.one.name}\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.one.username}"
+                    config.LOG_GROUP_ID, f"**» Assistant Started"
                 )
             except:
                 LOGGER(__name__).error(
@@ -78,8 +78,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("WoFBotsSupport")
-                await self.two.join_chat("WoFBotsSupport")
+                await self.two.join_chat("BotsXSupport")
+                await self.two.join_chat("BotsXSupport")
             except:
                 pass
             assistants.append(2)
@@ -95,7 +95,7 @@ class Userbot(Client):
                 self.two.name = get_me.first_name
             try:
                 await self.two.send_message(
-                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ᴀssɪsᴛᴀɴᴛ ᴛᴡᴏ sᴛᴀʀᴛᴇᴅ :**\n\n✨ ɪᴅ : `{self.two.id}`\n❄ ɴᴀᴍᴇ : {self.two.name}\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.two.username}"
+                    config.LOG_GROUP_ID, f"**» Assistant Stated"
                 )
             except:
                 LOGGER(__name__).error(
@@ -108,8 +108,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("WoFBotsSupport")
-                await self.three.join_chat("WoFBotsSupport")
+                await self.three.join_chat("BotsXSupport")
+                await self.three.join_chat("BotsXSupport")
             except:
                 pass
             assistants.append(3)
@@ -125,7 +125,7 @@ class Userbot(Client):
                 self.three.name = get_me.first_name
             try:
                 await self.three.send_message(
-                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ᴀssɪsᴛᴀɴᴛ ᴛʜʀᴇᴇ sᴛᴀʀᴛᴇᴅ :**\n\n✨ ɪᴅ : `{self.three.id}`\n❄ ɴᴀᴍᴇ : {self.three.name}\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.three.username}"
+                    config.LOG_GROUP_ID, f"**» Assistant Started"
                 )
             except:
                 LOGGER(__name__).error(
@@ -138,8 +138,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("WoFBotsSupport")
-                await self.four.join_chat("WoFBotsSupport")
+                await self.four.join_chat("BotsXSupport")
+                await self.four.join_chat("BotsXSupport")
             except:
                 pass
             assistants.append(4)
@@ -155,7 +155,7 @@ class Userbot(Client):
                 self.four.name = get_me.first_name
             try:
                 await self.four.send_message(
-                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ᴀssɪsᴛᴀɴᴛ ғᴏᴜʀ sᴛᴀʀᴛᴇᴅ :**\n\n✨ ɪᴅ : `{self.four.id}`\n❄ ɴᴀᴍᴇ : {self.four.name}\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.four.username}"
+                    config.LOG_GROUP_ID, f"**» Assistant Started"
                 )
             except:
                 LOGGER(__name__).error(
@@ -168,8 +168,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("WoFBotsSupport")
-                await self.five.join_chat("WoFBotsSupport")
+                await self.five.join_chat("BotsXSupport")
+                await self.five.join_chat("BotsXSupport")
             except:
                 pass
             assistants.append(5)
@@ -185,7 +185,7 @@ class Userbot(Client):
                 self.five.name = get_me.first_name
             try:
                 await self.five.send_message(
-                    config.LOG_GROUP_ID, f"**» {config.MUSIC_BOT_NAME} ᴀssɪsᴛᴀɴᴛ ғɪᴠᴇ sᴛᴀʀᴛᴇᴅ :**\n\n✨ ɪᴅ : `{self.five.id}`\n❄ ɴᴀᴍᴇ : {self.five.name}\n💫 ᴜsᴇʀɴᴀᴍᴇ : @{self.five.username}"
+                    config.LOG_GROUP_ID, f"**» Assistant Started"
                 )
             except:
                 LOGGER(__name__).error(
