030c7814d66ef95b4e75eb298cad186d64e89eb4
╭•Subaru• 神 <115202562+Aran-Sama@users.noreply.github.com>
2023-05-18 11:44:52 +0530

    Update logger.py

--- VIVI/plugins/Robot/logger.py
+++ VIVI/plugins/Robot/logger.py
@@ -2,10 +2,10 @@ from pyrogram import filters
 
 import config
 from strings import get_command
-from VIVI import app
-from VIVI.misc import SUDOERS
-from VIVI.utils.database import add_off, add_on
-from VIVI.utils.decorators.language import language
+from VelionaMusicBot import app
+from VelionaMusicBot.misc import SUDOERS
+from VelionaMusicBot.utils.database import add_off, add_on
+from VelionaMusicBot.utils.decorators.language import language
 
 # Commands
 LOGGER_COMMAND = get_command("LOGGER_COMMAND")
