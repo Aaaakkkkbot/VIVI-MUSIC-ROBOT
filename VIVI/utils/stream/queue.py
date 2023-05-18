8af7cf6f783703da448e930fed235d9410d648ee
╭•Subaru• 神 <115202562+Aran-Sama@users.noreply.github.com>
2023-05-18 14:28:46 +0530

    Update queue.py

--- VIVI/utils/stream/queue.py
+++ VIVI/utils/stream/queue.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from config import autoclean, chatstats, userstats
 from config.config import time_to_seconds
-from VIVI.misc import db
+from VelionaMusicBot.misc import db
 
 
 async def put_queue(
