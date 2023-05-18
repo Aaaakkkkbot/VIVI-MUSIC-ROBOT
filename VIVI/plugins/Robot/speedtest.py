9a9f5f796f0b89984cfb5d48ec615f2bcfb08c36
╭•Subaru• 神 <115202562+Aran-Sama@users.noreply.github.com>
2023-05-18 13:34:45 +0530

    Update speedtest.py

--- VIVI/plugins/Robot/speedtest.py
+++ VIVI/plugins/Robot/speedtest.py
@@ -2,8 +2,8 @@ import asyncio
 import speedtest
 from pyrogram import filters
 from strings import get_command
-from VIVI import app
-from VIVI.misc import SUDOERS
+from VelionaMusicBot import app
+from VelionaMusicBot.misc import SUDOERS
 
 # Commands
 SPEEDTEST_COMMAND = get_command("SPEEDTEST_COMMAND")
