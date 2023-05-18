4fc599b3732bb1156a5834a4df4ac34dc4cd7c64
╭•Subaru• 神 <115202562+Aran-Sama@users.noreply.github.com>
2023-05-18 14:55:00 +0530

    Update misc.py

--- VIVI/misc.py
+++ VIVI/misc.py
@@ -5,7 +5,7 @@ import heroku3
 from pyrogram import filters
 
 import config
-from VIVI.core.mongo import pymongodb
+from VelionaMusicBot.core.mongo import pymongodb
 
 from .logging import LOGGER
 
